251313d76043aa6a292c05f0996531ec9cb48394
Jelle Overbeek <j.overbeek@recognize.nl>
2018-03-15 14:16:28 +0100

    added gzip headers

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const nunjucks = require('nunjucks');
 const path = require('path');
 const routeStatic = require('./lib/route-static');
 const redirectIndices = require('./lib/redirect-indices');
+const compression = require('compression')
 
 const app = express();
 const baseDir = 'src';
@@ -10,6 +11,7 @@ const port = process.env.PORT || 3004;
 
 app.set('etag', false);
 app.use((req, res, next) => { res.removeHeader('X-Powered-By'); next(); });
+app.use(compression());
 
 // static routes
 app.use(routeStatic);
@@ -30,4 +32,5 @@ app.get('*', (req, res, next) => {
 app.listen(port, (err) => {
     err ? console.error(err) : console.log(`app running on http://localhost:${port}`);
 });
+
 
